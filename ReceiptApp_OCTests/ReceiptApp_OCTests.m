9f547b71cbb2a21fbda7d226f3af5f8c04b7647f
Adam Liu <adamliu-t@10501-DT-018.local>
2022-02-18 16:02:42 +0800

    3.0

---
+++ ReceiptApp_OCTests/ReceiptApp_OCTests.m
@@ -0,0 +1,36 @@
+//
+//  ReceiptApp_OCTests.m
+//  ReceiptApp_OCTests
+//
+//  Created by CAI SI LIOU on 2022/2/17.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ReceiptApp_OCTests : XCTestCase
+
+@end
+
+@implementation ReceiptApp_OCTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
